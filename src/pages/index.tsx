a41878577adcd527e4c08c79acdf1718a409b76e
bobochained <bobchained@gmail.com>
2023-04-07 15:51:06 +1200

    add partners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import type { GetStaticProps } from "next";
+import { Partner } from "../components/Partner";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
@@ -17,7 +18,7 @@ export default function Home() {
         <link rel="icon" href="/logo.png" />
       </Head>
       <main className="bg-black">
-        <section className="bg-secondary">
+        <section className="bg-secondary pb-36">
           <div className="container mx-auto ">
             <div className="flex justify-center">
               <Image src="/banner.png" alt="banner" width={1047} height={577} />
@@ -33,8 +34,8 @@ export default function Home() {
           </div>
         </section>
         {separator}
-        <section className="bg-tertiary">
-          <div className="container mx-auto ">
+        <section className="bg-tertiary py-20">
+          <div className="container mx-auto -mt-40">
             <div className="grid grid-cols-7 gap-4">
               {Array.from({ length: 22 }).map((_, index) => (
                 <Image
@@ -61,6 +62,41 @@ export default function Home() {
           </div>
         </section>
         {separator}
+        <section className="bg-secondary py-20">
+          <div className="container mx-auto flex flex-column justify-between">
+            <Partner
+              image="/nextdao.png"
+              href="https://twitter.com/thenextdao"
+              name="nextdao"
+            />
+            <Partner
+              image="/lxdao.png"
+              href="https://lxdao.io/"
+              name="lxdao"
+              width={175}
+              height={47}
+            />
+            <Partner
+              image="/hellonft.png"
+              href="https://hellonft.pro/"
+              name="hellonft"
+            />
+            <Partner
+              image="/smartdeer.png"
+              href="https://www.smartdeer.work/"
+              name="smartdeer"
+            />
+          </div>
+        </section>
+        {separator}
+        <section className="bg-tertiary py-20">
+          <div className="container mx-auto -mt-36 flex flex-col items-center">
+            <Image src="/ship.png" alt="ship" width={900} height={385} />
+            <p className="text-white">
+              All rights reserved by NextDAO © 2022 - 2023
+            </p>
+          </div>
+        </section>
       </main>
     </>
   );
