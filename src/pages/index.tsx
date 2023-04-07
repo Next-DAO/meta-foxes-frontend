ecbddd41b4f564127240cdbfec800567d273c49b
bobochained <bobchained@gmail.com>
2023-04-08 09:57:37 +1200

    responsive foxes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <link rel="icon" href="/logo.png" />
       </Head>
       <main className="bg-black">
-        <section className="bg-secondary pb-36">
+        <section className="bg-secondary pb-36 pointer-events-none">
           <div className="mx-auto pt-3 md:pt-9">
             <div className="flex justify-center">
               <div className="relative w-95 md:w-189 lg:w-250 aspect-[1000/577]">
@@ -37,27 +37,45 @@ export default function Home() {
         </section>
         {separator}
         <section className="bg-tertiary py-20">
-          <div className="container mx-auto -mt-40">
-            <div className="grid grid-cols-7 gap-4">
-              {Array.from({ length: 22 }).map((_, index) => (
-                <Image
+          <div className="flex justify-center -mt-40 ">
+            <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-1">
+              <div className="hidden lg:block xl:hidden w-[120px] md:w-[160px] aspect-square" />
+
+              <div className="relative w-[120px] md:w-[160px] aspect-square">
+                <Image src={`/foxes/fox0.png`} alt={`fox0`} fill />
+              </div>
+              <div className="relative w-[120px] md:w-[160px] aspect-square">
+                <Image src={`/foxes/fox1.png`} alt={`fox1`} fill />
+              </div>
+              <div className="relative w-[120px] md:w-[160px] aspect-square">
+                <Image src={`/foxes/fox2.png`} alt={`fox2`} fill />
+              </div>
+
+              <div className="hidden lg:block xl:hidden w-[120px] md:w-[160px] aspect-square" />
+              {Array.from({ length: 17 }).map((_, index) => (
+                <div
                   key={index}
-                  src={`/foxes/fox${index}.png`}
-                  alt={`fox${index}`}
-                  width={160}
-                  height={160}
-                />
-              ))}
-              <div
-                className={`${styles.customization} col-start-3 col-end-6 row-start-2 row-end-4 flex justify-center`}
-              >
-                <div className="-mt-6">
+                  className="relative w-[120px] md:w-[160px] aspect-square"
+                >
                   <Image
-                    src="/customization.png"
-                    alt="customization"
-                    width={432}
-                    height={315}
+                    src={`/foxes/fox${index + 3}.png`}
+                    alt={`fox${index + 3}`}
+                    fill
                   />
+                </div>
+              ))}
+              <div className="hidden lg:block relative w-[120px] md:w-[160px] aspect-square">
+                <Image src={`/foxes/fox20.png`} alt={`fox20`} fill />
+              </div>
+              <div className="md:hidden lg:block relative w-[120px] md:w-[160px] aspect-square">
+                <Image src={`/foxes/fox21.png`} alt={`fox21`} fill />
+              </div>
+              {/* TODO: use bg-gradient */}
+              <div
+                className={`${styles.customization} col-start-1 lg:col-start-2 xl:col-start-3 col-span-3 md:col-span-4 lg:col-span-3  row-start-5 md:row-start-4 xl:row-start-2 row-span-2 flex justify-center`}
+              >
+                <div className="relative w-[314px] md:w-[432px] aspect-[432/352] -mt-6 md:-mt-8">
+                  <Image src="/customization.png" alt="customization" fill />
                 </div>
               </div>
             </div>
