948d8109035645a76e513226cb6b3ba749947c2a
bobochained <bobchained@gmail.com>
2023-05-04 19:18:43 +1200

    move signatures files to frontend repo

--- src/hooks/useGetWhitelist.ts
+++ src/hooks/useGetWhitelist.ts
@@ -7,12 +7,16 @@ export type Signature = {
 
 type WhiteList = Record<string, Signature>;
 
+const signatureUrl = {
+  mainnet: "data/signatures.json",
+  goerli: "data/goerli_signatures.json",
+};
+
 export const useGetWhitelist = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [whitelist, setWhitelist] = useState<WhiteList | undefined>();
   useEffect(() => {
-    const timestamp = Date.now();
-    fetch(`${process.env.NEXT_PUBLIC_WHITELIST_URL!}?t=${timestamp}`)
+    fetch(signatureUrl[process.env.NEXT_PUBLIC_CHAIN])
       .then((res) => res.json())
       .then((data) => setWhitelist(data))
       .finally(() => setIsLoading(false));
