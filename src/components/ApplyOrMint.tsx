37d846eae70252211f6b68b676cb556a64df1b69
shep.eth <shep@nextdao.xyz>
2023-12-01 17:13:36 +1300

    use disabled

--- src/components/ApplyOrMint.tsx
+++ src/components/ApplyOrMint.tsx
@@ -22,7 +22,9 @@ export const ApplyOrMint = () => {
   if (!isWhitelisted) {
     return (
       <Button
+        href="https://0c5mmv5ifyn.typeform.com/to/Xsf4q1RX"
         title={t("customization")}
+        disabled
       />
     );
   }
